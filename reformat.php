314331f046b76b4e3cfcad25b933e1e2dea247f6
Kane Menicou <kanemenicou@gmail.com>
2020-06-22 17:42:30 +0100

    Fixes questions

--- reformat.php
+++ reformat.php
@@ -12,8 +12,41 @@ $finder = new Finder();
 $files = $finder->in(__DIR__ . '/data')->files()->name('/.*\.[yml|yaml]/');
 
 foreach ($files as $file) {
+    $content = Yaml::parse($file->getContents());
+
+    foreach ($content['questions'] ?? [] as $question) {
+        $oneIsCorrect = false;
+        $questionText = $question['question'];
+
+        foreach ($question['answers'] as $answer) {
+
+            if ( ! array_key_exists('correct', $answer)) {
+                echo "No 'correct' index for: " . $questionText . PHP_EOL;
+                continue;
+            }
+
+            if ($answer['correct'] === true) {
+                $oneIsCorrect = true;
+            }
+
+            if ( ! array_key_exists('value', $answer)) {
+                echo "No 'value' index for: " . $questionText . PHP_EOL;
+                continue;
+            }
+
+            if ( ! is_string($answer['value']) && ! is_int($answer['value']) && ! is_float($answer['value'])) {
+                echo "'value' index incorrect type: " . $questionText . PHP_EOL;
+                continue;
+            }
+        }
+
+        if ( ! $oneIsCorrect) {
+            echo 'no correct answer for: ' . $questionText . PHP_EOL;
+        }
+    }
+
     file_put_contents(
         $file->getRealPath(),
-        Yaml::dump(Yaml::parse($file->getContents()), 5, 4, Yaml::DUMP_MULTI_LINE_LITERAL_BLOCK)
+        Yaml::dump($content, 5, 4, Yaml::DUMP_MULTI_LINE_LITERAL_BLOCK)
     );
 }
